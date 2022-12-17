9914bb29bac175f28347c1b8822b91a57b632679
kovallski <lizkes651@gmail.com>
2022-12-17 14:19:23 +0300

    feat: add swiper and video.js for lab7

--- scripts/get_images.js
+++ scripts/get_images.js
@@ -1,17 +1,14 @@
-
 async function getResponse() {
   let response = await fetch('https://jsonplaceholder.typicode.com/photos');
   let content = await response.json();
   content = content.splice(0, 5);
   console.log(content);
-  let list = document.querySelector('.posts');
+  let list = document.querySelector('.swiper-wrapper');
   let key;
   for (key in content) {
-  list.innerHTML +=`<li class="post">
-      <h4>${content[key].title}</h4>
-    <img src="${content[key].url}" width="300">
-  </li>`
+    list.innerHTML +=  `<div class="swiper-slide"><img class="sec" src="${content[key].url}"></div>`
   }
-}
+    console.log(list);
 
+}
 getResponse().catch(err => alert("⚠ Что-то пошло не так"));
